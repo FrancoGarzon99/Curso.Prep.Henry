6ad4ab701683817a5598f8a9a4ccb4c1a3b38c9f
FrancoGarzon99 <57675375+FrancoGarzon99@users.noreply.github.com>
2020-06-16 17:45:43 -0300

    Update homework.js

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -2,23 +2,35 @@
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
+ return cb()
 }
 
 function sumarArray(numeros, cb) {
   // Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
-  // No es necesario devolver nada
+  // No es necesario devolver nada 
+  let suma = 0
+  numeros.forEach(element => {
+    suma += element
+  });
+  return cb(suma)
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
+  let listNumber = 0
+  array.forEach(element => {
+    listNumber = cb(element)
+  });
+  return listNumber
 }
 
 function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
+  return array.map(e => cb(e))
 }
 
 // No modificar nada debajo de esta línea
